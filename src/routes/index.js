46fe752c3489c84ad8f0ca4e18f33143ddc29731
rqhansen <1508477509@qq.com>
2020-05-19 10:43:25 +0800

    feat: 添加路由演示

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,36 @@
 const router = require('koa-router')()
 
-router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
-})
-
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
-
-router.get('/json', async (ctx, next) => {
+router.get('/profile/:userName', async (ctx,next) => {
+  const { userName } = ctx.params;
   ctx.body = {
-    title: 'koa2 json'
+    title: '哈',
+    userName: userName
   }
 })
+
+router.get('/loadMore/:userName/:pageIndex', async (ctx,next) => {
+  const { userName, pageIndex } = ctx.params;
+  ctx.body = {
+    title: 'this is loadMore api',
+    userName,
+    pageIndex
+  }
+})
+
+// router.get('/', async (ctx, next) => {
+//   await ctx.render('index', {
+//     title: 'Hello Koa 2!'
+//   })
+// })
+
+// router.get('/string', async (ctx, next) => {
+//   ctx.body = 'koa2 string'
+// })
+
+// router.get('/json', async (ctx, next) => {
+//   ctx.body = {
+//     title: 'koa2 json'
+//   }
+// })
 
 module.exports = router
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,12 +2,16 @@ const router = require('koa-router')()
 
 router.prefix('/users')
 
-router.get('/', function (ctx, next) {
-  ctx.body = 'this is a users response!'
+router.post('/login', async (ctx,next) => {
+  const { userName,password } = ctx.request.body;
+  ctx.body = {
+    userName,
+    password
+  }
 })
 
-router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
-})
+// router.get('/bar', function (ctx, next) {
+//   ctx.body = 'this is a users/bar response'
+// })
 
 module.exports = router
